33ec5ea485fa324f8bf947ec4d732fdec4dfdfc5
PHAROSWX <tombalesta123@gmail.com>
2026-07-27 02:11:25 +0300

    Create theme.js

---
+++ js/theme.js
@@ -0,0 +1,32 @@
+// Central place for tier colors + icon paths so the toolbar, legend,
+// renderer, and editor all agree on the same values.
+
+export const TIER_COLORS = {
+  low: "#f5f90b",   // < 40%
+  mid: "#ffa500",   // 40-60%
+  high: "#e0080a"   // > 60%
+};
+
+export const TIER_LABELS = {
+  low: "< 40%",
+  mid: "40\u201360%",
+  high: "> 60%"
+};
+
+export const ICONS = {
+  "x-low": "img/icons/x-low.png",
+  "x-mid": "img/icons/x-mid.png",
+  "x-high": "img/icons/x-high.png",
+  "arrow-low": "img/icons/arrow-low.png",
+  "arrow-mid": "img/icons/arrow-mid.png",
+  "arrow-high": "img/icons/arrow-high.png",
+  "storm": "img/icons/storm.png",
+  "major-storm": "img/icons/major-storm.png",
+  "remnants": "img/icons/remnants.png",
+  "potential-outline": "img/icons/potential-outline.png"
+};
+
+// Rough North Atlantic + Europe area of interest, used to trim world
+// topology to just what's relevant (better performance, no far-flung
+// landmasses distorting the Mercator projection at the edges).
+export const AOI_BOUNDS = { lonMin: -95, lonMax: 55, latMin: 10, latMax: 82 };
