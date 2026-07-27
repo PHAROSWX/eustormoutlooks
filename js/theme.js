15f3d4e767dae41d12065b7f2a26160f0331cb60
PHAROSWX <tombalesta123@gmail.com>
2026-07-28 02:41:40 +0300

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -38,6 +38,7 @@ export const SYSTEM_CLASSIFICATIONS = [
 export const TRACK_COLOR = "#2b2f33";
 export const CONE_COLOR = "#ffffff";
 export const DEFAULT_CONE_STEP_KM = 60;
+export const DEFAULT_CONE_SMOOTH = true;
 
 // Rough North Atlantic + Europe area of interest, used to trim world
 // topology to just what's relevant (better performance, no far-flung
